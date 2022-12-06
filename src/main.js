8034928a3264cbfc052bd2bf7dc386a8224fd682
LAPTOP-KHEUTB4N\lenovo <2608796981@qq.com>
2022-12-06 15:06:10 +0800

    fix:不同设备不同url

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from '../router/index'
 import './assets/font/iconfont.css'
+import getUserAgent from './plugin/getUserAgent'
 const app = createApp(App)
 const pinia = createPinia()
 app.use(router)
 app.use(pinia)
+app.use(getUserAgent)
 app.mount('#app')
---
+++ src/plugin/getUserAgent.js
@@ -0,0 +1,17 @@
+export default {
+    install: () => {
+        var browser = {
+            wapUrl: `${window.location.origin}/mobile.html#/`,
+            versions: function () {
+                var u = navigator.userAgent
+                return {
+                    //mobile: !!u.match(/AppleWebKit.*Mobile.*/)||!!u.match(/AppleWebKit/), //是否为移动终端
+                    mobile: u.match(/(iPhone|iPod|Android|ios|iPad)/i),
+                };
+            }()
+        }
+        if (browser.versions.mobile && browser.wapUrl != '') {
+            location.href = browser.wapUrl;
+        }
+    }
+}
